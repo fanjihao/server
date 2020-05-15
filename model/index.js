9628f6d9f2c02b74bad6b99b7b61811432bc92cf
meng <2562245299@qq.com>
2020-05-15 17:56:19 +0800

    5.15 meng

--- model/index.js
+++ model/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user:'root',
-  password: '244819',
+  password: 'root',
   database: 'xian.com',
   port: '3306'
 });
